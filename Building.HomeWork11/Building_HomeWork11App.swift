02daaf1546746138b0694a4080e2062a4cc6b556
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-09-14 15:13:35 +0200

    Initial Commit

---
+++ Building.HomeWork11/Building_HomeWork11App.swift
@@ -0,0 +1,17 @@
+//
+//  Building_HomeWork11App.swift
+//  Building.HomeWork11
+//
+//  Created by Авазбек Надырбек уулу on 14.09.25.
+//
+
+import SwiftUI
+
+@main
+struct Building_HomeWork11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

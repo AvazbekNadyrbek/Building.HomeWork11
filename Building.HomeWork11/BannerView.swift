32f0ea7f3c359c61e8a504a02d1eea3dda84be4a
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-09-19 16:00:08 +0200

    Here i am done modern UI, i have to finished also other part

---
+++ Building.HomeWork11/BannerView.swift
@@ -0,0 +1,18 @@
+//
+//  BannerView.swift
+//  Building.HomeWork11
+//
+//  Created by Авазбек Надырбек уулу on 16.09.25.
+//
+
+import SwiftUI
+
+struct BannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BannerView()
+}
